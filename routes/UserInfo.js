acaa699785659c6736bde84f162761a77c3fe32a
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-03-29 04:47:22 +0500

    fix - provided params to controllers

--- routes/UserInfo.js
+++ routes/UserInfo.js
@@ -4,7 +4,11 @@ import UserInfoController from "../controllers/UserInfoController.js";
 
 const routerUserInfo = new Router();
 
-routerUserInfo.post(ROUTE_USER_INFO, UserInfoController.create);
-routerUserInfo.get(ROUTE_USER_INFO, UserInfoController.getById);
+routerUserInfo.post(ROUTE_USER_INFO, (req, res) =>
+  UserInfoController.create(req, res)
+);
+routerUserInfo.get(ROUTE_USER_INFO, (req, res) =>
+  UserInfoController.getById(req, res)
+);
 
 export default routerUserInfo;
