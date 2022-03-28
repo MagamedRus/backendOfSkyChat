89fb105ea114c3496791651f9f516cfd86594198
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-03-29 04:43:09 +0500

    added sql connection config

--- constans/config.js
+++ constans/config.js
@@ -1,1 +1,7 @@
 export const PORT = process.env.PORT || 8081;
+export const MYSQL_CONFIG = {
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USERNAME || "root",
+  database: process.env.DB_NAME || "open_chat",
+  password: process.env.DB_PASSWORD || "root",
+};
