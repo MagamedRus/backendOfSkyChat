443802f82c56d710d5baeb206134da786f64255d
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-03-31 13:59:27 +0500

    validate for userInfoValidations

---
+++ common/reqValidations/userInfoValidations.js
@@ -0,0 +1,48 @@
+import {
+  isValidEMail,
+  isValidLogin,
+  isValidName,
+  isValidDate,
+  isValidPassword,
+  isValidBirthplace,
+} from "../validations.js";
+import {
+  INCORRECT_BIRTH_PLACE,
+  INCORRECT_EMAIL,
+  INCORRECT_LOGIN,
+  INCORRECT_FIRST_NAME,
+  INCORRECT_SECOND_NAME,
+  INCORRECT_LAST_NAME,
+  INCORRECT_BIRTHDATE,
+  INCORRECT_REGISTRATION_DATE,
+  INCORRECT_PASSWORD,
+} from "../../constans/types/exceptions.js";
+
+export const validUserInfoPostReq = (userInfoObj) => {
+  let errorType = null;
+
+  if (!isValidLogin(userInfoObj.login)) {
+    errorType = INCORRECT_LOGIN;
+  } else if (!isValidEMail(userInfoObj.email)) {
+    errorType = INCORRECT_EMAIL;
+  } else if (!isValidName(userInfoObj.firstName)) {
+    errorType = INCORRECT_FIRST_NAME;
+  } else if (!isValidName(userInfoObj.secondName)) {
+    errorType = INCORRECT_SECOND_NAME;
+  } else if (userInfoObj.lastName && !isValidName(userInfoObj.lastName)) {
+    errorType = INCORRECT_LAST_NAME;
+  } else if (!isValidDate(userInfoObj.registrationDate)) {
+    errorType = INCORRECT_REGISTRATION_DATE;
+  } else if (!isValidDate(userInfoObj.birthdate)) {
+    errorType = INCORRECT_BIRTHDATE;
+  } else if (!isValidPassword(userInfoObj.password)) {
+    errorType = INCORRECT_PASSWORD;
+  } else if (
+    userInfoObj.birthPlace &&
+    !isValidBirthplace(userInfoObj.password)
+  ) {
+    errorType = INCORRECT_BIRTH_PLACE;
+  }
+
+  return errorType;
+};
--- constans/types/exceptions.js
+++ constans/types/exceptions.js
@@ -1,1 +1,11 @@
 export const NOT_FOUND_ID_EXCEPTION = "param 'id' is not found";
+export const INCORRECT_EMAIL = "param 'email' is incorrect";
+export const INCORRECT_LOGIN = "param 'login' is incorrect";
+export const INCORRECT_FIRST_NAME = "param 'firstName' is incorrect";
+export const INCORRECT_SECOND_NAME = "param 'secondName' is incorrect";
+export const INCORRECT_LAST_NAME = "param 'email' is incorrect";
+export const INCORRECT_BIRTHDATE = "param 'birthdate' is incorrect";
+export const INCORRECT_REGISTRATION_DATE =
+  "param 'registrationDate' is incorrect";
+export const INCORRECT_BIRTH_PLACE = "param 'birthPlace' is incorrect";
+export const INCORRECT_PASSWORD = "param 'password' is incorrect";
