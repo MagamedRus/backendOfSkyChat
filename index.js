5d789b8fcb5bb55ee610d706d02e86eab67a1ec4
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-02 19:26:15 +0500

    included cors

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ import { PORT } from "./constans/config.js";
 import routerUserInfo from "./routes/UserInfo.js";
 import routerCheckData from "./routes/CheckData.js";
 import { ROUTE_API } from "./constans/routes.js";
+import cors from "cors";
+import morgan from "morgan";
 
 // express config
 const app = express();
 app.use(express.json());
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(morgan("dev"));
 app.use(ROUTE_API, routerUserInfo);
 app.use(ROUTE_API, routerCheckData);
 
