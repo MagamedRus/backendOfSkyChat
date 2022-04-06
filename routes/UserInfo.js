9b71a353859211bc39b9afbe256e9d3b5a282633
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-06 16:24:23 +0500

    renamed route path

--- routes/UserInfo.js
+++ routes/UserInfo.js
@@ -3,23 +3,24 @@ import {
   ROUTE_USER_INFO,
   ROUTE_USER_INFO_BY_ID,
   ROUTE_USER_INFO_BY_EMAIL,
+  ROUTE_USER_NEW,
 } from "../constans/routes.js";
 import UserInfoController from "../controllers/UserInfoController.js";
 
 const routerUserInfo = new Router();
 const userController = new UserInfoController();
 
-routerUserInfo.post(ROUTE_USER_INFO, (req, res) =>
+routerUserInfo.post(ROUTE_USER_NEW, (req, res) =>
   userController.create(req, res)
 );
 routerUserInfo.get(ROUTE_USER_INFO, (req, res) =>
   userController.getAll(req, res)
 );
-routerUserInfo.get(ROUTE_USER_INFO_BY_ID, (req, res) =>
+routerUserInfo.post(ROUTE_USER_INFO_BY_ID, (req, res) =>
   userController.getById(req, res)
 );
 
-routerUserInfo.get(ROUTE_USER_INFO_BY_EMAIL, (req, res) =>
+routerUserInfo.post(ROUTE_USER_INFO_BY_EMAIL, (req, res) =>
   userController.getByEmail(req, res)
 );
 
