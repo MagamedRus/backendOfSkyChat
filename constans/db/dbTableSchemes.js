20257b3d72d67ca3bae884741e41619269f058f5
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-09 21:28:32 +0500

    scheme of new chat

--- constans/db/dbTableSchemes.js
+++ constans/db/dbTableSchemes.js
@@ -1,3 +1,4 @@
 export const userDataTableScheme =
   "(`id`, `login`, `email`, `password`, `firstName`, `secondName`, `lastName`, `registrationDate`, `birthPlace`, `birthday`)";
-  
+export const chatTableScheme =
+  "(`id`, `adminsId`, `title`, `usersId`, `chatHistory`, `createDate`)";
