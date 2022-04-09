06052ca0bccd2c0e7f2d6b3e6f738044ebdf893e
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-09 21:27:42 +0500

    new routes path for chat

--- constans/routes.js
+++ constans/routes.js
@@ -5,8 +5,10 @@ export const ROUTE_USER_INFO = "/user/info";
 export const ROUTE_USER_INFO_BY_ID = "/user/info/id";
 export const ROUTE_USER_INFO_BY_EMAIL = "/user/info/email";
 export const ROUTE_REG = "/registration";
-export const ROUTE_CHECK= "/check";
+export const ROUTE_CHECK = "/check";
 export const ROUTE_USER_EMAIL = "/user/email";
 export const ROUTE_USER_LOGIN = "/user/login";
 export const ROUTE_USER_AUTH = "/user/auth";
-
+export const ROUTE_CHAT_NEW = "/chat/new";
+export const ROUTE_CHAT_HEADERS = "/chat/headers";
+export const ROUTE_CHAT_DATA = "/chat/data";
