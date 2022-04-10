da80d52a356290c36792c3e89c41a68a22687c3c
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-11 03:47:48 +0500

    add test request

--- routes/Chats.js
+++ routes/Chats.js
@@ -9,14 +9,15 @@ import ChatController from "../controllers/ChatController.js";
 const routerChat = new Router();
 const chatController = new ChatController();
 
-routerChat.post(ROUTE_CHAT_NEW, (req, res) =>
-  chatController.newChat(req, res)
-);
+routerChat.post(ROUTE_CHAT_NEW, (req, res) => chatController.newChat(req, res));
 routerChat.post(ROUTE_CHAT_HEADERS, (req, res) =>
   chatController.getChatHeadersData(req, res)
 );
 routerChat.post(ROUTE_CHAT_DATA, (req, res) =>
   chatController.getChatData(req, res)
+);
+routerChat.post("/message/new", (req, res) =>
+  chatController.updateMessageData(req, res)
 );
 
 export default routerChat;
