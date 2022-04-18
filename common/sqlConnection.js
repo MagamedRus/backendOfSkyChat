9011a98b3eb03dd22c724237b19551b8966db715
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-18 21:26:35 +0500

    add new sql2 module for sync req

--- common/sqlConnection.js
+++ common/sqlConnection.js
@@ -1,6 +1,11 @@
 import mysql from "mysql";
+import promiseMySql from 'mysql2/promise';
 import { MYSQL_CONFIG } from "../constans/config.js";
 
 export const getDBConn = () => {
   return mysql.createPool({ connectionLimit: 10, ...MYSQL_CONFIG });
+};
+
+export const getSyncDBConn = () => {
+  return promiseMySql.createConnection(MYSQL_CONFIG);
 };
