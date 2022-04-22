11310cae9f1b208d84937d5f68fd017dc0076df9
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-04-23 01:50:48 +0500

    fix/add import modeles to changeMessageData common

--- common/chat.js
+++ common/chat.js
@@ -1,17 +1,19 @@
+import { getDateInMilliseconds } from "./date.js";
+import { isEmptyString } from "./validations.js";
+
 export function changeMessageData(messagesData, newMessage) {
   let messagesList = isEmptyString(messagesData)
     ? []
     : JSON.parse(messagesData);
   let result = new Array(messagesList);
-  if (newMessage.id != undefined) {
+  if (newMessage?.id != undefined) {
     const existMessageIndex = messagesList.findIndex(
       (el) => el.id == newMessage.id
     );
     const isExist = existMessageIndex !== -1;
     if (isExist) {
       const isSameMessage =
-        messagesList[existMessageIndex].messageText ===
-        newMessage.messageText;
+        messagesList[existMessageIndex].messageText === newMessage.messageText;
       if (!isSameMessage) {
         messagesList[existMessageIndex] = {
           ...messagesList[existMessageIndex],
