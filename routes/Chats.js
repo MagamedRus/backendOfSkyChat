8ee54b6a40c5cfc30841b621c8dca27d9195014e
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-05-11 15:01:53 +0500

    add new req of getPrivateChatId

--- routes/Chats.js
+++ routes/Chats.js
@@ -3,6 +3,7 @@ import {
   ROUTE_CHAT_NEW,
   ROUTE_CHAT_HEADERS,
   ROUTE_CHAT_DATA,
+  ROUTE_PRIVATE_CHAT_ID
 } from "../constans/routes.js";
 import ChatController from "../controllers/ChatController.js";
 
@@ -15,5 +16,9 @@ routerChat.post(ROUTE_CHAT_HEADERS, (req, res) =>
 );
 routerChat.post(ROUTE_CHAT_DATA, (req, res) =>
   chatController.getChatDataById(req, res)
+);
+
+routerChat.post(ROUTE_PRIVATE_CHAT_ID, (req, res) =>
+  chatController.getPrivateChatId(req, res)
 );
 export default routerChat;
