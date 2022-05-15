8cf03111599e9e2a8caed5b1a7a8e8eddc105a3b
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-05-15 22:09:19 +0500

    add create img scheme and req

--- constans/db/dbTableSchemes.js
+++ constans/db/dbTableSchemes.js
@@ -1,7 +1,7 @@
 export const userSelfDataTableScheme =
   "(`id`, `login`, `email`, `password`, `firstName`, `secondName`, `lastName`, `registrationDate`, `birthPlace`, `birthday`)";
 export const chatTableScheme =
-  "(`id`, `adminsId`, `title`, `usersId`, `chatHistory`, `createDate`, `isGeneral`, `lastChangeDate`, `isAdminChat`)";
+  "(`id`, `adminsId`, `title`, `usersId`,`imageId`, `chatHistory`, `createDate`, `isGeneral`, `lastChangeDate`, `isAdminChat`)";
 export const tempDataTableScheme = "(`id`, `lastActiveTime`, `deviceToken`)";
 export const userDataTableScheme =
   "(`id`, `selfDataId`, `tempDataId`, `notificationsDataId`, `userFriendsDataArr`, `userChatsDataArr`)";
@@ -11,3 +11,4 @@ export const userFriendsTableScheme =
   "(`id`, `friendId`, `createdTime`, `isAccept`)";
 export const newFriendsNotifications =
   "(`id`, `userId`, `friendId`, `createdDate`, `isRead`)";
+export const imagesTableScheme = "(`id`, `smallImage`, `image`)";
