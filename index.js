e5f808ac1f47a2e978f368e920e366658c836963
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-05-23 15:47:06 +0500

    add limit of requests size

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ const app = express();
 const _cors = cors({
   origin: "*",
 });
-app.use(express.json());
-app.use(morgan("dev"));
+app.use(express.json({limit: '5mb'}));
 app.use(_cors);
 app.use(morgan("dev"));
 app.use(ROUTE_API, routerUserInfo);
